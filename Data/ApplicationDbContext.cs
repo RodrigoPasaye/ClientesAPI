945ba42b5cffc2b06470519cc0bee202be572724
RodrigoPasaye <cv6200@innovaccion.mx>
2022-10-14 21:13:08 -0500

    Se crea clase para el DbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ClientesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClientesAPI.Data {
+    public class ApplicationDbContext : DbContext {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
