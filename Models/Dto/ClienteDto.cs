7b006d81a2a8319f3fd47eb30172ca7437b580c5
RodrigoPasaye <cv6200@innovaccion.mx>
2022-10-16 17:11:00 -0500

    Se agrega clase para el patron DTO

---
+++ Models/Dto/ClienteDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClientesAPI.Models.Dto {
+    public class ClienteDto {
+        public int Id { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+    }
+}
---
+++ Models/Dto/ResponseDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ClientesAPI.Models.Dto {
+    public class ResponseDto {
+        public bool IsSuccess { get; set; } = true;
+        public object Result { get; set; }
+        public string DisplayMessage { get; set; }
+        public List<string> ErrorMessages { get; set; }
+    }
+}
