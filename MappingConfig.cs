87204c5a18c0916ecc82e0bac99f4bc635a03f91
RodrigoPasaye <cv6200@innovaccion.mx>
2022-10-16 17:22:36 -0500

    Se agrega clase para hacer el mapeo entre los DTO y los modelos

---
+++ MappingConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ClientesAPI.Models;
+using ClientesAPI.Models.Dto;
+
+namespace ClientesAPI {
+    public class MappingConfig {
+        public static MapperConfiguration RegisterMaps() {
+            var mappingConfig = new MapperConfiguration(config => {
+                config.CreateMap<ClienteDto, Cliente>();
+                config.CreateMap<Cliente, ClienteDto>();
+            });
+            return mappingConfig;
+        }
+    }
+}
