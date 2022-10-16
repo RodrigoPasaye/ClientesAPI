0fe39e62b8a6c92f8d3d5b5ac894e916483cfdb9
RodrigoPasaye <cv6200@innovaccion.mx>
2022-10-16 17:07:50 -0500

    Se agrega interface que contiene los metodos que se mandan a llamar cuando se hace un request

---
+++ Repositorio/IClienteRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using ClientesAPI.Models.Dto;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ClientesAPI.Repositorio {
+    public interface IClienteRepositorio {
+        Task<List<ClienteDto>> GetClientes();
+        Task<ClienteDto> GetClienteById(int id);
+        Task<ClienteDto> CreateUpdate(ClienteDto clienteDto);
+        Task<bool> DeleteCliente(int id);
+    }
+}
