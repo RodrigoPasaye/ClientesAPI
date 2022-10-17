14eef59911754e5925b4161fd550fb6cd5a433e5
RodrigoPasaye <cv6200@innovaccion.mx>
2022-10-16 19:53:26 -0500

    Se agrega interfaz del repositorio para el modelo de User

---
+++ Repositorio/IUserRepositorio.cs
@@ -0,0 +1,10 @@
+﻿using ClientesAPI.Models;
+using System.Threading.Tasks;
+
+namespace ClientesAPI.Repositorio {
+    public interface IUserRepositorio {
+        Task<int> Register(User user, string password);
+        Task<string> Login(string userName, string password);
+        Task<bool> UserExist(string userName);
+    }
+}
