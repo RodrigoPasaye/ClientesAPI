c7f98fd2468e6be40334c42ca8f1fd78405f3b0a
RodrigoPasaye <cv6200@innovaccion.mx>
2022-10-16 20:08:04 -0500

    Se agrega controlador de Users y se crea el primer metodo para registrar nuevos usuarios

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using ClientesAPI.Models;
+using ClientesAPI.Models.Dto;
+using ClientesAPI.Repositorio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ClientesAPI.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase {
+        private readonly IUserRepositorio _userRepositorio;
+        protected ResponseDto _responseDto;
+        public UsersController(IUserRepositorio userRepositorio) {
+            _userRepositorio = userRepositorio;
+            _responseDto = new ResponseDto();
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult> Register(UserDto userDto) {
+            var respuesta = await _userRepositorio.Register(new User {
+                UserName = userDto.UserName
+            }, userDto.Password);
+            if (respuesta == -1) {
+                _responseDto.IsSuccess = false;
+                _responseDto.DisplayMessage = "El Usuario ya Existe";
+                return BadRequest(_responseDto);
+            }
+            if (respuesta == -500) {
+                _responseDto.IsSuccess = false;
+                _responseDto.DisplayMessage = "Error al Crear el Usuario";
+                return BadRequest(_responseDto);
+            }
+            _responseDto.DisplayMessage = "Usuario Creado con Éxito";
+            _responseDto.Result = respuesta;
+            return Ok(_responseDto);
+        }
+    }
+}
